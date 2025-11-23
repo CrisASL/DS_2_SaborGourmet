38593054a1687b6f7bf9cc0004b49ff2d2fa5d8e
Cristian <cristian.salas.liz@gmail.com>
2025-11-23 00:46:47 -0300

    Mejora visual 3

---
+++ src/main/java/cl/ipss/eva2/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package cl.ipss.eva2.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index"; // carga el dashboard
+    }
+}
